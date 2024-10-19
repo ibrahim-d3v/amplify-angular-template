54d50e95c53e66589593b69b3d06d134f87dc75f
ibra <imahmoud94@hotmail.com>
2024-10-19 13:35:06 +0300

    add env

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,8 +9,8 @@ export const auth = defineAuth({
     email: true,
     externalProviders: {
       google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        clientId: secret('144906124225-8em4dattokr58qjhnegndfarkgd3i2n6.apps.googleusercontent.com'),
+        clientSecret: secret('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'),
         scopes: ['email', 'profile', 'openid'],
         attributeMapping: {
           email: 'email'
